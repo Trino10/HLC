8f975e9bf7fba99b17ca0da16f32d2a7d47a77b2
Trino10 <eljuma10@gmail.com>
2023-11-29 11:01:52 +0000

    primer commit

---
+++ web/index.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <?php echo "<h1>Holiiiiiiiii</h1>"?>
+        <marquee><?php echo "Soy el Trino que pasa tio"?></marquee>
+    </body>
+</html>
---
+++ web/info/src/usuarios.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <?php echo "<h1>Holiiiiiiiii</h1>"?>
+        <h1><?php echo "Listado de usuarios"?></h1>
+    </body>
+</html>
